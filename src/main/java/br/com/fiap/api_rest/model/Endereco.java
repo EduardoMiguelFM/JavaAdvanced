8e75fffb164787e6c2fb9c5e97a7b944b4667606
dudumfmonteiro@gmail.com <labsfiap@fiap.com.br>
2025-03-21 11:45:04 -0300

    aula 21-03-2025

--- src/main/java/br/com/fiap/api_rest/model/Endereco.java
+++ src/main/java/br/com/fiap/api_rest/model/Endereco.java
@@ -1,21 +1,23 @@
 package br.com.fiap.api_rest.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 public class Endereco {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
+    @NotBlank(message = "O texto da localização é obrigatório")
     private String localizacao;
-    @OneToOne(mappedBy = "endereco", cascade = CascadeType.REMOVE)
+    @OneToOne(mappedBy = "endereco")
     private Filial filial;
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
--- src/main/java/br/com/fiap/api_rest/model/Filial.java
+++ src/main/java/br/com/fiap/api_rest/model/Filial.java
@@ -2,23 +2,32 @@ package br.com.fiap.api_rest.model;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 @Entity
 public class Filial {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
     private String nome;
     @OneToOne
     @JoinColumn(name = "id_endereco")
     private Endereco endereco;
+    @OneToMany(mappedBy = "filial")
+    private List<Cliente> clientes;
 
+    public Filial() {}
 
+    public Filial(String nome, Endereco endereco) {
+        this.nome = nome;
+        this.endereco = endereco;
+    }
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
@@ -36,5 +45,13 @@ public class Filial {
 
     public void setEndereco(Endereco endereco) {
         this.endereco = endereco;
+    }
+
+    public List<Cliente> getClientes() {
+        return clientes;
+    }
+
+    public void setClientes(List<Cliente> clientes) {
+        this.clientes = clientes;
     }
 }
